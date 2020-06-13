3094d1787d95a2f0674025b7e379f46c951b48b7
kiana1317 <kiana.hobbs@gmail.com>
2020-06-13 02:26:28 -0500

    server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 // express necessary for api
 const express = require('express');
-
 //api setup
 const api = express();
+// middleware function
+api.use(express.static(__dirname + '/public'));
+
+
 api.listen(3000, () => {
   console.log('API up and running!');
 });
 
 //route setup
-api.get('/', (req, res) => {
-  console.log(req);
-  res.send('Hello, world!');
-});
+// api.get('/', (req, res) => {
+//   console.log(req);
+//   res.send('Hello, world!');
+// });
